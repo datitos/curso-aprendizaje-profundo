b49fe60ea2818a1141137fe0395ba8ee87c8ddd4
matiasbattocchia <matias@riseup.net>
2021-01-30 12:01:14 +0000

    Deploying to gh-pages from  @ 4333612de0be16f1fe6c3162799803edec1c5c3a 🚀

--- scripts/md2html.js
+++ scripts/md2html.js
@@ -2,7 +2,13 @@ const fg = require('fast-glob')
 const fs = require('fs')
 const showdown = require('showdown')
 
-const args = process.argv;
+const args = process.argv
+
+const targetDir = args[3] ?? '.'
+
+if (!fs.existsSync(targetDir)){
+  fs.mkdirSync(targetDir)
+}
 
 const converter = new showdown.Converter({ headerLevelStart: 2 })
 
@@ -12,7 +18,7 @@ fg.sync(args[2]).forEach(path => {
   const text = fs.readFileSync(path, 'utf8')
   const html = converter.makeHtml(text)
 
-  const file = path.replace(/\.\w+$/, '.html')
+  const filename = path.replace(/\.\w+$/, '.html')
 
-  fs.writeFileSync(file, html)
+  fs.writeFileSync(`${targetDir}/${filename}`, html)
 })
